5645172589e47bebc72dddc03e17d6a5aa44723c
HoangThuc <HoangThuc@DESKTOP-C7OMQQ8>
2021-06-07 01:01:35 +0700

    update all

--- src/authentication/PrivateRoute.tsx
+++ src/authentication/PrivateRoute.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
-const PrivateRouteV2 = ({ component, ...rest }: any) => {
-  const routeComponent = (props: any) =>
-    localStorage.getItem('user') ? (
-      React.createElement(component, props)
-    ) : (
-      <Redirect to={{ pathname: '/login' }} />
-    )
-  return <Route {...rest} render={routeComponent} />
+import {Route, Redirect} from 'react-router-dom'
+import jwt_decode from "jwt-decode";
+
+const USERNAME_DEFAULT = "Est Rouge Test"
+const PrivateRouteV2 = ({component, ...rest}: any) => {
+    const ValidJWT = () => {
+        var temp = localStorage.getItem('user');
+        if (temp === null) {
+            return false;
+        } else {
+            let user = JSON.parse(temp ? temp : '');
+            var decoded: {
+                user: {
+                    username: string;
+                }
+            } = jwt_decode(user.token);
+            console.log(decoded);
+            if ((decoded.user.username === USERNAME_DEFAULT)) {
+                return true
+            } else {
+                return false
+            }
+
+        }
+
+    }
+
+    const routeComponent = (props: any) =>
+        ValidJWT() ? (
+            React.createElement(component, props)
+        ) : (
+            <Redirect to={{pathname: '/login'}}/>
+        )
+    return <Route {...rest} render={routeComponent}/>
 }
-export { PrivateRouteV2 }
+export {PrivateRouteV2}
