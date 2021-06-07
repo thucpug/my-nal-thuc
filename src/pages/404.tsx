ae13018738a1b5d5db9a211cafaf96efa9334b16
thuchd <thuchd@vnpt.vn>
2021-06-07 08:37:05 +0700

    update page 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,9 @@
 export default () => {
-  return <h1>404 Not found</h1>
+  return  <div className="mainbox">
+    <div className="err">4</div>
+    <i className="far fa-question-circle fa-spin"></i>
+    <div className="err2">4</div>
+    <div className="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="/home">home</a> and try from there.</p>
+    </div>
+  </div>
 }
